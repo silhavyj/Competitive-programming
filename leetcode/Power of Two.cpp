35dde3641cedebde0f77d0e6c68f8e24add936ed
silhavyj <64095079+silhavyj@users.noreply.github.com>
2020-06-08 10:38:31 +0200

    Update Power of Two.cpp

--- leetcode/Power of Two.cpp
+++ leetcode/Power of Two.cpp
@@ -1,17 +1,6 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if (n == 0)
-            return false;
-        int res = 0;
-        while (n) {
-            if (n & 1) {
-                res++;
-                if (res == 2)
-                    return false;
-            }
-            n >>= 1;
-        }
-        return true;
+        return n > 0 && !(n & (n-1));
     }
 };
