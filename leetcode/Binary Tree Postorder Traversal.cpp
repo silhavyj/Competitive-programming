6cfa4976ebc20d7e9d32080b89f9b0f72c2dec5a
silhavyj <jakub.sil@seznam.cz>
2021-06-20 18:33:21 +0200

    Create Binary Tree Postorder Traversal.cpp

---
+++ leetcode/Binary Tree Postorder Traversal.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        stack<pair<int,TreeNode *>> s;
+        vector<int> ans;
+        s.push({0,root});
+        while (!s.empty()) {
+            auto t = s.top();
+            s.pop();
+            if (t.second==NULL)
+                continue;
+            if (t.first==0) {
+                s.push({1,t.second});
+                s.push({0,t.second->right});
+                s.push({0,t.second->left});
+            } else if (t.first==1) {
+                ans.push_back(t.second->val);
+            }
+        }
+        return ans;
+    }
+};
