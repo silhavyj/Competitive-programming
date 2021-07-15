54f66a564798d0fde9e97c616fd6229b1ce3c3c0
silhavyj <jakub.sil@seznam.cz>
2021-07-15 11:00:00 +0200

    Create Valid Triangle Number.cpp

---
+++ leetcode/Valid Triangle Number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int triangleNumber(vector<int>& nums) {
+        int ans = 0;
+        sort(nums.begin(), nums.end());
+        for (int i = nums.size()-1; i >= 0; i--) {
+            int l=0, r=i-1;
+            while (l < r) {
+                if (nums[l]+nums[r] > nums[i]) {
+                    ans += r-l;
+                    r--;
+                } else {
+                    l++;
+                }
+            }
+        }
+        return ans;
+    }
+};
