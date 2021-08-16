962a3246e2ae7f3261a694f946593b8724033969
Kuba Šilhavý <jakub.sil@seznam.cz>
2021-08-16 09:58:32 +0200

    Update Range Sum Query - Immutable.cpp

--- leetcode/Range Sum Query - Immutable.cpp
+++ leetcode/Range Sum Query - Immutable.cpp
@@ -1,22 +1,22 @@
 class NumArray {
+    vector<int> sums;
 public:
-    
-    vector<int> nums;
-    
     NumArray(vector<int>& nums) {
-        this->nums = nums;
+        sums = vector<int>(nums.size(), 0);
+        sums[0] = nums[0];
+        for (int i = 1; i < nums.size(); i++)
+            sums[i] = nums[i] + sums[i-1];
     }
     
-    int sumRange(int i, int j) {
-        int sum = 0;
-        for (int k = i; k <= j; k++)
-            sum += nums[k];
-        return sum;
+    int sumRange(int left, int right) {
+        if (left == 0)
+            return sums[right];
+        return sums[right] - sums[left-1];
     }
 };
 
 /**
  * Your NumArray object will be instantiated and called as such:
  * NumArray* obj = new NumArray(nums);
- * int param_1 = obj->sumRange(i,j);
+ * int param_1 = obj->sumRange(left,right);
  */
