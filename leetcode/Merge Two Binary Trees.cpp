4a7ef6298664ebb069d1c73e46cc34aaee9c4167
Kuba Šilhavý <jakub.sil@seznam.cz>
2022-02-08 17:37:27 +0100

    Create Merge Two Binary Trees.cpp

---
+++ leetcode/Merge Two Binary Trees.cpp
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
+        if (root1 == NULL) return root2;
+        if (root2 == NULL) return root1;
+        int val = root1->val + root2->val;
+        TreeNode *node = new TreeNode(val);
+        node->left = mergeTrees(root1->left, root2->left);
+        node->right = mergeTrees(root1->right, root2->right);
+        return node;
+    }
+};
